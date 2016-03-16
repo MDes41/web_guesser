b4cf4b9fc062326657051b81ccbe8baeb2e1a35a
Matt DesMarteau <MDes41@users.noreply.github.com>
2016-03-15 22:29:55 -0600

    number of guesses and resets in 5 guesses

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,26 +1,38 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-NUMBER = rand(100)
 
 get '/' do
   guess = params["guess"].to_i
   message, color = check_guess(guess)
-  erb :index, :locals => { :number => NUMBER, :message => message, :color => color }
+  erb :index, :locals => { :number => @@NUMBER, :message => message, :color => color }
+end
+
+@@NUMBER = rand(100)
+@@guesses = 0
+
+def randomize
+  @@guesses = 0
+  @@NUMBER = rand(100)
 end
 
 def check_guess(guess)
-  if guess > NUMBER + 5
-    [ "Way to high!", "FF2400" ]
-  elsif guess > NUMBER
-    [ "Too High!", "F75D59" ]
-  elsif guess == 0
-    [ "Please make a valid guess", "8EEBEC" ]
-  elsif  guess < NUMBER - 5
-    [ "Way too low!", "FF2400" ]
-  elsif guess < NUMBER && guess != 0
-    [ "Too Low!", "F75D59" ]
-  elsif guess == NUMBER
-    [ "You got it right! The SERECT NUMBER is #{NUMBER}", "4CC417" ]
+  if guess > @@NUMBER + 5 && @@guesses != 5
+    @@guesses += 1
+    [ "Way to high!, you have #{5 - @@guesses} left", "FF2400" ]
+  elsif guess > @@NUMBER && @@guesses != 5
+    @@guesses += 1
+    [ "Too High!, you have #{5 - @@guesses} left", "F75D59" ]
+  elsif guess == 0 || @@guesses == 5
+    randomize
+    [ "Please make a valid guess a number between 0 and 100, you have #{5 - @@guesses} left", "8EEBEC" ]
+  elsif  guess < @@NUMBER - 5 && @@guesses != 5
+    @@guesses += 1
+    [ "Way too low!, you have #{5 - @@guesses} left", "FF2400" ]
+  elsif guess < @@NUMBER && @@guesses != 5
+    @@guesses += 1
+    [ "Too Low!, you have #{5 - @@guesses} left", "F75D59" ]
+  elsif guess == @@NUMBER && @@guesses != 5
+    [ "You got it right, in #{@@guesses + 1} guesses! The SERECT NUMBER is #{@@NUMBER}", "4CC417" ]
   end
 end

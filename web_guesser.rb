b1091239b7f3770a8cc7d6e1c4f0ef10b6307357
Matt DesMarteau <MDes41@users.noreply.github.com>
2016-03-15 19:21:56 -0600

    message for different guesses

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,8 +1,26 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-number = rand(100)
+NUMBER = rand(100)
 
 get '/' do
-  erb :index, :locals => { :number => number }
+  guess = params["guess"].to_i
+  message = check_guess(guess)
+  erb :index, :locals => { :number => NUMBER, :message => message }
+end
+
+def check_guess(guess)
+  if guess > NUMBER + 5
+    "Way to high!"
+  elsif guess > NUMBER
+    "Too High!"
+  elsif guess == 0
+    "Please make a valid guess"
+  elsif  guess < NUMBER - 5
+    "Way too low!"
+  elsif guess < NUMBER && guess != 0
+    "Too Low!"
+  elsif guess == NUMBER
+    "You got it right! The SERECT NUMBER is #{NUMBER} "
+  end
 end

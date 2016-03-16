174a7209b92a6aaa5b5ef34a691022381cd0d44d
Matt DesMarteau <MDes41@users.noreply.github.com>
2016-03-15 21:15:16 -0600

    colors for background extension

--- web_guesser.rb
+++ web_guesser.rb
@@ -5,22 +5,22 @@ NUMBER = rand(100)
 
 get '/' do
   guess = params["guess"].to_i
-  message = check_guess(guess)
-  erb :index, :locals => { :number => NUMBER, :message => message }
+  message, color = check_guess(guess)
+  erb :index, :locals => { :number => NUMBER, :message => message, :color => color }
 end
 
 def check_guess(guess)
   if guess > NUMBER + 5
-    "Way to high!"
+    [ "Way to high!", "FF2400" ]
   elsif guess > NUMBER
-    "Too High!"
+    [ "Too High!", "F75D59" ]
   elsif guess == 0
-    "Please make a valid guess"
+    [ "Please make a valid guess", "8EEBEC" ]
   elsif  guess < NUMBER - 5
-    "Way too low!"
+    [ "Way too low!", "FF2400" ]
   elsif guess < NUMBER && guess != 0
-    "Too Low!"
+    [ "Too Low!", "F75D59" ]
   elsif guess == NUMBER
-    "You got it right! The SERECT NUMBER is #{NUMBER} "
+    [ "You got it right! The SERECT NUMBER is #{NUMBER}", "4CC417" ]
   end
 end

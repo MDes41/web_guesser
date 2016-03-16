3c721205a27a1292267c668a2a7cebb36ce20356
Matt DesMarteau <MDes41@users.noreply.github.com>
2016-03-15 22:59:14 -0600

    working with resetting guesses when running out

--- web_guesser.rb
+++ web_guesser.rb
@@ -17,22 +17,32 @@ def randomize
 end
 
 def check_guess(guess)
-  if guess > @@NUMBER + 5 && @@guesses != 5
+  if guess > @@NUMBER + 5 && still_has_guesses && input_not_blank(guess)
     @@guesses += 1
-    [ "Way to high!, you have #{5 - @@guesses} left", "FF2400" ]
-  elsif guess > @@NUMBER && @@guesses != 5
+    [ "Way to high!, you have #{5 - @@guesses} guesses left", "FF2400" ]
+  elsif guess > @@NUMBER && still_has_guesses && input_not_blank(guess)
     @@guesses += 1
-    [ "Too High!, you have #{5 - @@guesses} left", "F75D59" ]
-  elsif guess == 0 || @@guesses == 5
-    randomize
-    [ "Please make a valid guess a number between 0 and 100, you have #{5 - @@guesses} left", "8EEBEC" ]
-  elsif  guess < @@NUMBER - 5 && @@guesses != 5
+    [ "Too High!, you have #{5 - @@guesses} guesses left", "F75D59" ]
+  elsif  guess < @@NUMBER - 5 && still_has_guesses && input_not_blank(guess)
     @@guesses += 1
-    [ "Way too low!, you have #{5 - @@guesses} left", "FF2400" ]
-  elsif guess < @@NUMBER && @@guesses != 5
+    [ "Way too low!, you have #{5 - @@guesses} guesses left", "FF2400" ]
+  elsif guess < @@NUMBER && still_has_guesses && input_not_blank(guess)
     @@guesses += 1
-    [ "Too Low!, you have #{5 - @@guesses} left", "F75D59" ]
-  elsif guess == @@NUMBER && @@guesses != 5
+    [ "Too Low!, you have #{5 - @@guesses} guesses left", "F75D59" ]
+  elsif guess == @@NUMBER && still_has_guesses && input_not_blank(guess)
     [ "You got it right, in #{@@guesses + 1} guesses! The SERECT NUMBER is #{@@NUMBER}", "4CC417" ]
+  elsif @@guesses == 0 || !still_has_guesses || !input_not_blank(guess)
+    lose_message = "Please guess a number between 0 and 100, you have #{5 - @@guesses} guesses before it resets to a new number"
+    lose_message = "You ran out of guesses, a new number has been generated, guess again" if @@guesses == 5
+    randomize if @@guesses == 5
+    [ lose_message, "8EEBEC" ]
   end
+end
+
+def input_not_blank(guess)
+  guess != 0 ? true : false
+end
+
+def still_has_guesses
+  @@guesses != 5
 end
